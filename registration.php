d608d69b53a5854edfce37f21ea5595aada3a556
Wafula James <33344724+Jameswafy@users.noreply.github.com>
2023-04-29 14:11:59 +0300

    Add files via upload
    Creating a new module named ‘Magebit_PageListWidget’.

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+This is My Registration Code to Create and Register the Module
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magebit_PageListWidget',
+    __DIR__
+);
